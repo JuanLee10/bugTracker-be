de1db79bc0163a50ab0ff27d9463c2480e38e304
Code Mode <codemode@Juans-MacBook-Pro.local>
2022-07-08 16:10:13 -0700

    changedenvironmental

--- index.js
+++ index.js
@@ -14,17 +14,15 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // => allows use to access the req.body
 
-app.use("/todo", todoRoutes);
+app.use("/todos", todoRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  //server static content
-  //npm run build
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   //server static content
+//   //npm run build
+//   app.use(express.static("client/build"));
+// }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server is starting on port ${PORT}`);
